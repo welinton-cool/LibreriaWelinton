7697a8f7a7dcda72d47a636b506f6cf73e8a7a0a
welinton-cool <welinton.sacon@espoch.edu.ec>
2025-10-23 18:09:43 -0500

    hola

---
+++ src/main/java/ec/edu/ec/libreriasaconc/LibreriaSaconC.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package ec.edu.ec.libreriasaconc;
+
+/**
+ *
+ * @author HP
+ */
+public class LibreriaSaconC {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
